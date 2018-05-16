562736afbdc5bfd04356ff1337cb7e433a036b5c
Yanko Yankov <jankoyangue@yahoo.com>
2018-05-16 18:51:26 +0300

    03 Lecture

---
+++ Programming_Fundamentals_05.2018/03_IntroAndBasicSyntax/01_Greeting/Greeting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01_Greeting
+{
+    class Greeting
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("What's your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
+
+
---
+++ Programming_Fundamentals_05.2018/03_IntroAndBasicSyntax/03_EmployeeData/EmployeeData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _03_EmployeeData
+{
+    class EmployeeData
+    {
+        static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int employeeID = int.Parse(Console.ReadLine());
+            decimal monthlySalary = decimal.Parse(Console.ReadLine());
+            decimal roundedMonthlySalary = Math.Round(monthlySalary, 2, MidpointRounding.AwayFromZero);
+
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Employee ID: {employeeID:D8}");
+            Console.WriteLine($"Salary: {roundedMonthlySalary:0.00}");
+
+        }
+    }
+}
