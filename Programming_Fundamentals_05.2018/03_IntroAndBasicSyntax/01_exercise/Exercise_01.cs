c47aa5a5339708921ff259e193157a3847fb9ea1
Yanko Yankov <jankoyangue@yahoo.com>
2018-05-16 17:11:20 +0300

    Zero exercise

---
+++ Programming_Fundamentals_05.2018/03_IntroAndBasicSyntax/01_exercise/Exercise_01.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01_exercise
+{
+    class Exercise_01
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hi");
+            string name = Console.ReadLine();
+            Console.WriteLine($"{name}");
+        }
+    }
+}
